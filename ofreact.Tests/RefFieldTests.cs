a3b286d40623c1d75c4c9ff9beecb12197090d46
phosphene47 <phosphene47@chiya.dev>
2020-02-12 22:40:38 +1100

    Add support for ref-bound fields

---
+++ ofreact.Tests/RefFieldTests.cs
@@ -0,0 +1,70 @@
+using NUnit.Framework;
+
+namespace ofreact.Tests
+{
+    public class RefFieldTests
+    {
+        class Element1 : ofComponent
+        {
+            public static int CurrentCount;
+            public static string CurrentString;
+
+            [Ref] public readonly RefObject<int> Count;
+            [Ref] public readonly RefObject<string> String;
+
+            protected override ofElement Render()
+            {
+                CurrentCount  = ++Count.Current;
+                CurrentString = String.Current = Count.Current.ToString();
+
+                return null;
+            }
+        }
+
+        [Test]
+        public void Ref()
+        {
+            Assert.That(Element1.CurrentCount, Is.EqualTo(0));
+            Assert.That(Element1.CurrentString, Is.Null);
+
+            using var node = new ofNodeRoot();
+
+            node.RenderElement(new Element1());
+
+            Assert.That(Element1.CurrentCount, Is.EqualTo(1));
+            Assert.That(Element1.CurrentString, Is.EqualTo("1"));
+
+            node.Invalidate();
+            node.RenderElement(new Element1());
+
+            Assert.That(Element1.CurrentCount, Is.EqualTo(2));
+            Assert.That(Element1.CurrentString, Is.EqualTo("2"));
+        }
+
+        class Element2 : ofComponent
+        {
+            public static bool Rendered;
+
+            [Ref(777)] public readonly RefObject<int> Number;
+
+            protected override ofElement Render()
+            {
+                Assert.That(Number.Current, Is.EqualTo(777));
+
+                Rendered = true;
+
+                return null;
+            }
+        }
+
+        [Test]
+        public void InitialValue()
+        {
+            using var node = new ofNodeRoot();
+
+            node.RenderElement(new Element2());
+
+            Assert.That(Element2.Rendered, Is.True);
+        }
+    }
+}
---
+++ ofreact/RefAttribute.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ofreact
+{
+    /// <summary>
+    /// Marks a field as a ref of an element.
+    /// </summary>
+    /// <remarks>
+    /// Ref fields must be public and the type must be a constructed <see cref="RefObject{T}"/>.
+    /// Refs do not cause a rerender even if its value changes.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class RefAttribute : Attribute, IElementFieldBinder
+    {
+        readonly object _initialValue;
+
+        FieldInfo _field;
+        string _name;
+
+        Func<ofNode, string, object> _createRef;
+
+        /// <summary>
+        /// Creates a new <see cref="RefAttribute"/>.
+        /// </summary>
+        public RefAttribute() { }
+
+        /// <summary>
+        /// Creates a new <see cref="RefAttribute"/> with the given initial value.
+        /// </summary>
+        /// <param name="initialValue">Initial value of the referenced value.</param>
+        public RefAttribute(object initialValue)
+        {
+            _initialValue = initialValue;
+        }
+
+        public void Initialize(FieldInfo field)
+        {
+            if (field.FieldType.GetGenericTypeDefinition() != typeof(RefObject<>))
+                throw new ArgumentException($"Field {field} of {field.DeclaringType} is not type of {typeof(RefObject<>)}");
+
+            _field = field;
+            _name  = field.Name.ToLowerInvariant();
+
+            var refCtor = field.FieldType.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)[0];
+            var type    = field.FieldType.GenericTypeArguments[0];
+
+            if (InternalConstants.IsEmitAvailable)
+            {
+                var node = Expression.Parameter(typeof(ofNode), "node");
+                var key  = Expression.Parameter(typeof(string), "key");
+
+                _createRef = Expression.Lambda<Func<ofNode, string, object>>(
+                                            Expression.New(
+                                                refCtor,
+                                                node,
+                                                key,
+                                                _initialValue == null
+                                                    ? Expression.Default(type) as Expression
+                                                    : Expression.Convert(Expression.Constant(_initialValue), type)),
+                                            node,
+                                            key)
+                                       .Compile();
+            }
+            else
+            {
+                var initial = _initialValue;
+
+                if (initial == null && type.IsValueType)
+                    initial = Activator.CreateInstance(type);
+
+                _createRef = (n, k) => refCtor.Invoke(new[] { n, k, initial });
+            }
+        }
+
+        public void Bind(ofElement element) => _field.SetValue(element, _createRef(element.Node, _name));
+    }
+}
