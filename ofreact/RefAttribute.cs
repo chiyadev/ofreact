96d7b339f81f94ac37219382b2cf1268a7ff5581
phosphene47 <phosphene47@chiya.dev>
2020-02-12 22:58:22 +1100

    Allow code reuse of ref/state object creation

--- ofreact/RefAttribute.cs
+++ ofreact/RefAttribute.cs
@@ -35,6 +35,41 @@ namespace ofreact
             _initialValue = initialValue;
         }
 
+        /// <summary>
+        /// Used to create a container dynamically.
+        /// </summary>
+        /// <param name="container">Either <see cref="RefObject{T}"/> or <see cref="StateObject{T}"/> constructed.</param>
+        /// <param name="initial">Initial value.</param>
+        /// <returns>Function to create it.</returns>
+        internal static Func<ofNode, string, object> CreateRefOrStateObject(Type container, object initial)
+        {
+            var refCtor = container.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)[0];
+            var type    = container.GenericTypeArguments[0];
+
+            if (InternalConstants.IsEmitAvailable)
+            {
+                var node = Expression.Parameter(typeof(ofNode), "node");
+                var key  = Expression.Parameter(typeof(string), "key");
+
+                return Expression.Lambda<Func<ofNode, string, object>>(
+                                      Expression.New(
+                                          refCtor,
+                                          node,
+                                          key,
+                                          initial == null
+                                              ? Expression.Default(type) as Expression
+                                              : Expression.Convert(Expression.Constant(initial), type)),
+                                      node,
+                                      key)
+                                 .Compile();
+            }
+
+            if (initial == null && type.IsValueType)
+                initial = Activator.CreateInstance(type);
+
+            return (n, k) => refCtor.Invoke(new[] { n, k, initial });
+        }
+
         public void Initialize(FieldInfo field)
         {
             if (field.FieldType.GetGenericTypeDefinition() != typeof(RefObject<>))
@@ -43,35 +78,7 @@ namespace ofreact
             _field = field;
             _name  = field.Name.ToLowerInvariant();
 
-            var refCtor = field.FieldType.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)[0];
-            var type    = field.FieldType.GenericTypeArguments[0];
-
-            if (InternalConstants.IsEmitAvailable)
-            {
-                var node = Expression.Parameter(typeof(ofNode), "node");
-                var key  = Expression.Parameter(typeof(string), "key");
-
-                _createRef = Expression.Lambda<Func<ofNode, string, object>>(
-                                            Expression.New(
-                                                refCtor,
-                                                node,
-                                                key,
-                                                _initialValue == null
-                                                    ? Expression.Default(type) as Expression
-                                                    : Expression.Convert(Expression.Constant(_initialValue), type)),
-                                            node,
-                                            key)
-                                       .Compile();
-            }
-            else
-            {
-                var initial = _initialValue;
-
-                if (initial == null && type.IsValueType)
-                    initial = Activator.CreateInstance(type);
-
-                _createRef = (n, k) => refCtor.Invoke(new[] { n, k, initial });
-            }
+            _createRef = CreateRefOrStateObject(_field.FieldType, _initialValue);
         }
 
         public void Bind(ofElement element) => _field.SetValue(element, _createRef(element.Node, _name));
