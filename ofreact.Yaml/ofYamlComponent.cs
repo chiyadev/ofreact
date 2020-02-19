519766e5e1bc9121f74ab89875178826a47d8ab9
phosphene47 <phosphene47@chiya.dev>
2020-02-19 19:38:01 +1100

    Add a helper ofYamlComponent

---
+++ ofreact.Yaml/ofYamlComponent.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using YamlDotNet.RepresentationModel;
+using static ofreact.Hooks;
+
+namespace ofreact.Yaml
+{
+    /// <summary>
+    /// Represents a dynamic component that is created from an YAML document.
+    /// </summary>
+    /// <remarks>
+    /// This is a helper component that wraps <see cref="YamlComponentBuilder"/>.
+    /// </remarks>
+    public class ofYamlComponent : ofComponent
+    {
+        readonly Func<FunctionComponent> _build;
+
+        /// <summary>
+        /// Creates a new <see cref="ofYamlComponent"/> from an YAML document.
+        /// </summary>
+        public ofYamlComponent(YamlDocument document, object key = default) : base(key)
+        {
+            _build = new YamlComponentBuilder(document).BuildRenderer;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ofYamlComponent"/> from a reader.
+        /// </summary>
+        /// <remarks>
+        /// <paramref name="reader"/> must not be disposed until this element is rendered at least once.
+        /// Caller is responsible for disposing the reader afterwards.
+        /// </remarks>
+        public ofYamlComponent(TextReader reader, object key = default) : base(key)
+        {
+            _build = () =>
+            {
+                var stream = new YamlStream();
+
+                stream.Load(reader);
+
+                return new YamlComponentBuilder(stream.Documents[0]).BuildRenderer();
+            };
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ofYamlComponent"/> from a document string.
+        /// </summary>
+        public ofYamlComponent(string document, object key = default) : this(new StringReader(document), key) { }
+
+        protected override ofElement Render() => UseMemo(_build)(Node);
+    }
+}
