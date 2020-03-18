9d1f000b7a47cd665b95e12cd6aa6a7afc32f61a
phosphene47 <phosphene47@chiya.dev>
2020-03-18 15:52:33 +1100

    Add hook for scheduling

--- osu.Framework.Declarative/Hooks.cs
+++ osu.Framework.Declarative/Hooks.cs
@@ -1,4 +1,6 @@
 using osu.Framework.Allocation;
+using osu.Framework.Platform;
+using osu.Framework.Threading;
 using static ofreact.Hooks;
 
 namespace osu.Framework.Declarative
@@ -6,5 +8,6 @@ namespace osu.Framework.Declarative
     public static class Hooks
     {
         public static T UseDependency<T>() where T : class => UseContext<IDrawableRenderContext>()?.DependencyContainer.Get<T>();
+        public static Scheduler UseScheduler() => UseDependency<GameHost>().UpdateThread.Scheduler;
     }
 }
